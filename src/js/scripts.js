34795ba8fc0c801c072288f5be888e115635af44
zoeaanderson <zanderso@bu.edu>
2023-12-05 17:53:23 -0500

    12/5
    pre-data

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -42,14 +42,15 @@ async function getAirtableData() {
 const button = document.querySelector('.buttonone');
 
 
-document.querySelector(".buttonone").addEventListener('click', getQuizOne);
+document.querySelector('.buttonone').addEventListener('click', getQuizOne);
 function getQuizOne() {
-	var section = document.querySelector("#quizone");
-	section.classList.add("show");
+	var section = document.querySelector('#quizone');
+	section.classList.toggle('show');
 
 	var section = document.querySelector("#homepage");
-	section.classList.add("hide");
+	section.classList.toggle('hide');
 }
+
 
 const checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
 
@@ -58,3 +59,69 @@ checkboxes.forEach(checkbox => {
 });
 
 
+
+document.querySelector('.buttontwo').addEventListener('click', getQuizTwo);
+function getQuizTwo() {
+
+	var section = document.querySelector('#quiztwo');
+	section.classList.toggle('show');
+
+	var section = document.querySelector("#quizone");
+	section.classList.toggle('hide');
+
+}
+
+const radios = document.querySelectorAll('input[type="radio"]');
+radios.forEach(radio => {
+	radio.name = "adjectives";
+	radio.addEventListener('change', () => {
+	});
+});
+
+document.querySelector('.buttonthree').addEventListener('click', getQuizThree);
+function getQuizThree() {
+
+	var section = document.querySelector('#quizthree');
+	section.classList.toggle('show');
+
+	var section = document.querySelector("#quiztwo");
+	section.classList.toggle('hide');
+
+}
+
+document.querySelector('.buttonfour').addEventListener('click', getQuizFour);
+function getQuizFour() {
+
+	var section = document.querySelector('#quizfour');
+	section.classList.toggle('show');
+
+	var section = document.querySelector("#quizthree");
+	section.classList.toggle('hide');
+
+}
+
+document.querySelector('.buttonfive').addEventListener('click', getResultsPage);
+function getResultsPage() {
+
+	var section = document.querySelector('#resultspage');
+	section.classList.toggle('show');
+
+	var section = document.querySelector("#quizfour");
+	section.classList.toggle('hide');
+
+}
+
+document.querySelector('.restart').addEventListener('click', alertRestart);
+function alertRestart() {
+	var txt;
+	if (confirm("Are you sure you want to restart the quiz?")) {
+		var section = document.querySelector('#homepage');
+		section.classList.toggle('show');
+
+		var section = document.querySelector("#quizone #quizthree #quizfour #resultspage");
+		section.classList.toggle('hide');
+	} else {
+
+	}
+
+}
